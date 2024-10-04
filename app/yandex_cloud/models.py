2ebbd6e5a43f67ba9a2fee530959539fde67d995
Denis <n3m0_22@mail.ru>
2024-10-05 00:47:07 +0300

    Updata yandex_cloud

--- app/yandex_cloud/models.py
+++ app/yandex_cloud/models.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 
 
 class File:
@@ -7,22 +7,32 @@ class File:
     download_dir: str = "downloads"
 
     def __init__(
-        self, public_key: str, name: str, _type: str, download_url: Optional[str]
+        self,
+        public_key: str,
+        name: str,
+        _type: str,
+        download_url: Optional[str],
+        children: Optional[List["File"]] = None,
     ):
         self.public_key = public_key
         self.name = name
         self.type = _type
         self.download_url = download_url
+        self.children = children
 
     @classmethod
     def from_json(cls, data: Dict[str, Any]) -> "File":
         """Create a File instance from a JSON-like dictionary."""
+        children = [
+            cls.from_json(item) for item in data.get("_embedded", {}).get("items", [])
+        ]
         return cls(
             public_key=data["public_key"],
             name=data["name"],
             _type=data["type"],
             download_url=data.get("file"),
+            children=children,
         )
 
     def __repr__(self):
-        return f"File(pulic_key={self.public_key}, name={self.name}, type={self.type}, download_url={self.download_url})"
+        return f"File(pulic_key={self.public_key}, name={self.name}, type={self.type}, download_url={self.download_url}, children={self.children})"
