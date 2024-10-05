57d9b1913db1e3342efc70a3c5fbf6fbf5641ad8
Denis <n3m0_22@mail.ru>
2024-10-05 22:35:09 +0300

    Update logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,10 +1,15 @@
+import os
 from loguru import logger
 
 
 def setup_logger():
+
+    # Проверка существования директории для логов
+    os.makedirs("logs", exist_ok=True)
+
     logger.add(
-        "app.log",
-        rotation="1 MB",
+        "logs/app.log",
+        rotation="1 week",
         level="DEBUG",
         format="{time} {level} {message}",
         backtrace=True,
