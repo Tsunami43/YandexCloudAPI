4d055ee354358d56a67317e02a5d79d3c2679ecf
Denis <n3m0_22@mail.ru>
2024-10-05 20:25:17 +0300

    Updata yandex_cloud

--- app/yandex_cloud/api.py
+++ app/yandex_cloud/api.py
@@ -9,10 +9,14 @@ class YandexCloudAPIClient:
     def __init__(self):
         self.base_url = "https://cloud-api.yandex.net/v1/disk/public/resources"
 
-    async def get_files(self, public_key: str) -> Optional[List[File]]:
+    async def get_files(
+        self, public_key: str, path: Optional[str] = None
+    ) -> Optional[List[File]]:
         """Retrieve the list of files from a public link."""
         async with ClientSession() as session:
             params = {"public_key": public_key}
+            if path:
+                params["path"] = path
             try:
                 async with session.get(self.base_url, params=params) as response:
                     response.raise_for_status()
@@ -26,29 +30,23 @@ class YandexCloudAPIClient:
 
             return None
 
-    async def download_file(self, file: File):
-        """Download this file using a context manager for session and save it in the specified directory structure."""
-        if file.download_url is None:
-            raise ValueError("'download_url' is None")
-
-        os.makedirs(file.download_dir, exist_ok=True)  # parent dir for downloads
-        file_path = os.path.join(file.download_dir, file.name)
-
-        async with ClientSession() as session:
+    async def download_file(self, download_url: str) -> bytes:
+        async with ClientSession() as session:  # Создаем асинхронную сессию
             try:
-                async with session.get(file.download_url) as response:
-                    response.raise_for_status()
-                    content = await response.read()
-
-                    with open(file_path, "wb") as f:
-                        f.write(content)
-
+                async with session.get(
+                    download_url
+                ) as response:  # Отправляем GET-запрос на скачивание
+                    response.raise_for_status()  # Проверяем статус ответа
+                    content = await response.read()  # Читаем содержимое файла
                     logger.info(
-                        f"File '{file.name}' successfully downloaded and saved to {file_path}"
-                    )
+                        f"Successfully downloaded '{download_url}'."
+                    )  # Логируем успешное скачивание
+                    return content  # Возвращаем содержимое файла в памяти
             except ClientError as e:
-                logger.error(f"Network error while downloading file '{file.name}': {e}")
+                logger.error(f"Network error while downloading  '{download_url}': {e}")
             except Exception as e:
                 logger.error(
-                    f"An error occurred while downloading file '{file.name}': {e}"
+                    f"An error occurred while downloading '{download_url}': {e}"
                 )
+
+        return None  # Если произошла ошибка, возвращаем None
--- app/yandex_cloud/models.py
+++ app/yandex_cloud/models.py
@@ -1,8 +1,32 @@
-from typing import Dict, Any, Optional, List
+from typing import Dict, Any, Optional
+
+mime_types = {
+    "Изображения": [
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/bmp",
+        "image/webp",
+    ],
+    "Документы": [
+        "application/pdf",
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.ms-excel",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    ],
+    "Видеофайлы": [
+        "video/mp4",
+        "video/x-msvideo",
+        "video/x-flv",
+        "video/quicktime",
+    ],
+    "Аудиофайлы": ["audio/mpeg", "audio/wav", "audio/ogg"],
+}
 
 
 class File:
-    """Model representing a file from Yandex Disk."""
+    """Модель, представляющая собой файл с Яндекс Диска."""
 
     download_dir: str = "downloads"
 
@@ -11,28 +35,40 @@ class File:
         public_key: str,
         name: str,
         _type: str,
+        path: str,
         download_url: Optional[str],
-        children: Optional[List["File"]] = None,
+        mime_type: Optional[str] = None,  # Добавляем mime_type
     ):
         self.public_key = public_key
         self.name = name
         self.type = _type
         self.download_url = download_url
-        self.children = children
+        self.path = path
+        self.mime_type = mime_type  # Присваиваем mime_type атрибуту
+
+    @property
+    def is_folder(self):
+        return self.type == "dir"
 
     @classmethod
     def from_json(cls, data: Dict[str, Any]) -> "File":
-        """Create a File instance from a JSON-like dictionary."""
-        children = [
-            cls.from_json(item) for item in data.get("_embedded", {}).get("items", [])
-        ]
+        """Создайте экземпляр файла из словаря, подобного JSON."""
         return cls(
             public_key=data["public_key"],
             name=data["name"],
             _type=data["type"],
+            path=data["path"],
             download_url=data.get("file"),
-            children=children,
+            mime_type=data.get("mime_type"),  # Извлекаем mime_type из данных
         )
 
+    def is_valid_mime_type(self, selected_type: str) -> bool:
+        """Проверяет, соответствует ли mime_type выбранному типу."""
+        if selected_type == "Все файлы":
+            return True
+        if selected_type in mime_types:
+            return self.mime_type in mime_types[selected_type]
+        return False
+
     def __repr__(self):
-        return f"File(pulic_key={self.public_key}, name={self.name}, type={self.type}, download_url={self.download_url}, children={self.children})"
+        return f"File(public_key={self.public_key}, name={self.name}, type={self.type}, mime_type={self.mime_type}, download_url={self.download_url}, path={self.path})"
